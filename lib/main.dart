b51c1dda3a8cc197cfd672e21dfe5e792a3dc6de
rkrp15 <rkrp15@gmail.com>
2023-05-26 16:30:02 +0900

    .

--- lib/main.dart
+++ lib/main.dart
@@ -20,49 +20,34 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
+class MyHomePage extends StatelessWidget {
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      body: const Column(children: [
+        Text('Hello, Text Widget!',
+            style: TextStyle(
+                fontSize: 25,
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.w700)),
+        Text('Hello, Text Widget!',
+            style: TextStyle(
+                fontSize: 25,
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.w700)),
+        Text('Hello, Text Widget!',
+            style: TextStyle(
+                fontSize: 25,
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.w700))
+      ]),
     );
   }
 }
